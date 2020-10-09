e9c651749ef11da81c3a0949537cb3ad51f060c4
Vostrikov Nikita <vostrikov.nikita@lll.kpi.ua>
2020-10-09 20:36:00 +0300

    Little changes

--- NeuralNetwork/apps/neuralmain/bin/ReLearningSystem.ps1
+++ NeuralNetwork/apps/neuralmain/bin/ReLearningSystem.ps1
@@ -1,3 +1,3 @@
-#2020-05-08 13:46:46
+#2020-10-09 18:18:42
 python.exe F:\Projects\PyCharm\NeuralNetwork\NeuralNetwork\apps\neuralmain\bin\scripts\retrain.py --output_graph=F:\Projects\PyCharm\NeuralNetwork\NeuralNetwork\apps\neuralmain\bin\tf_files\retrained_graph.pb --output_labels=F:\Projects\PyCharm\NeuralNetwork\NeuralNetwork\apps\neuralmain\bin\tf_files\retrained_labels.txt --image_dir=F:\Projects\PyCharm\NeuralNetwork\NeuralNetwork\apps\neuralmain\bin\tf_files\leaf_photos
 Sleep -Seconds 300
